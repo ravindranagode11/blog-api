528c32a81b8fa118c6fb391583e6a0f1bf08c6d6
unknown <ravindranagode11@gmail.com>
2023-10-15 21:35:25 +0530

    modifire file

--- src/main/java/comcom/codewithdurgesg/blog/blogappapis/BlogAppApisApplication.java
+++ src/main/java/comcom/codewithdurgesg/blog/blogappapis/BlogAppApisApplication.java
@@ -2,12 +2,14 @@ package comcom.codewithdurgesg.blog.blogappapis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@RestController 
 @SpringBootApplication
 public class BlogAppApisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 	}
+
+ 
 
 }
